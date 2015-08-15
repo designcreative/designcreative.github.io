80816ab7c25e7537fc025ebcb5ac16aa6952e92f
Alexander Rizhikoff <designcreative.me@gmail.com>
2015-08-15 11:19:03 +0300

    Refactor index

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 'use strict';
 
 var dcApp = angular.module('dcApp', ['ngRoute', 'dcControllers']);
+
+dcApp.config(['$routeProvider', '$locationProvider'], function($routeProvider, $locationProvider){
+	$routeProvider.when('/', {
+			templateUrl: 'partials/main.html',
+			controller: 'MainCtrl'
+		}).when('/show', {
+			templateUrl: 'partials/show.html',
+			controller: 'ShowCtrl'
+		});
+});
