09385896a9d3138ac0c891ddc664a1e9cfd84dbe
Alexander Rizhikoff <designcreative.me@gmail.com>
2015-08-15 22:48:44 +0300

    Add views for single item and routes

--- js/app.js
+++ js/app.js
@@ -2,12 +2,21 @@
 
 var dcApp = angular.module('dcApp', ['ngRoute', 'dcControllers']);
 
-dcApp.config(['$routeProvider', '$locationProvider'], function($routeProvider, $locationProvider){
-	$routeProvider.when('/', {
-			templateUrl: 'partials/main.html',
-			controller: 'MainCtrl'
-		}).when('/show', {
-			templateUrl: 'partials/show.html',
-			controller: 'ShowCtrl'
+dcApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+	$routeProvider.
+		when('/', {
+			templateUrl: 'partials/home.html',
+			controller: 'TemplatesListCtrl'
+		}).
+		when('/templates/:templateId', {
+			templateUrl: 'partials/templates.html',
+			controller: 'TemplateDetailsCtrl'
 		});
-});
+
+		
+		$locationProvider.html5Mode({
+			enabled: true,
+			requireBase: true
+		});
+
+}]);
--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,14 @@
-var dcApp = angular.module('dcApp', []);
+var dcControllers = angular.module('dcControllers', []);
 
-dcApp.controller('TemplatesListCtrl', function($scope, $http) {
+dcControllers.controller('TemplatesListCtrl', ['$scope','$http',function($scope, $http) {
 	$http.get('data/templates.json').success(function(data){
 		$scope.templates = data;
 	});
 	
 	$scope.orderProp = 'id';
-});
+}]);
+
+dcControllers.controller('TemplateDetailsCtrl', ['$scope', '$routeParams', 
+	function($scope, $routeParams){
+		$scope.templateId = $routeParams.templateId;
+	}]);
